16a674e92386abb88d5f601f38275fe9eb7fb33f
WitchDevelops <d.n.wojewska@gmail.com>
2025-11-30 16:32:56 +0100

    refactor: clean up styles

--- scripts/swiperInit.js
+++ scripts/swiperInit.js
@@ -8,20 +8,10 @@
     loop: true,
     autoHeight: true,
 
-    // If we need pagination
-    pagination: {
-      el: root.querySelector(".es-swiper-pagination"),
-    },
-
     // Navigation arrows
     navigation: {
       nextEl: root.querySelector(".es-swiper-button-next"),
       prevEl: root.querySelector(".es-swiper-button-prev"),
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: root.querySelector(".es-swiper-scrollbar"),
     },
   });
 })();
